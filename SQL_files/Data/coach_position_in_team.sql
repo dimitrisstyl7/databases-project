702bc34f53d31b674ba8fc854d2c05be080a8f57
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-05-23 13:20:19 +0300

    coach_position_in_team data file updated.

--- SQL_files/Data/coach_position_in_team.sql
+++ SQL_files/Data/coach_position_in_team.sql
@@ -1,10 +1,10 @@
 insert into footballclub_db.public.coach_position_in_team (coach_id, team_id, position)
 values (1, 1, 'goalkeeping');
 insert into footballclub_db.public.coach_position_in_team (coach_id, team_id, position)
-values (2, 1, 'goalkeeping');
+values (2, 2, 'goalkeeping');
 insert into footballclub_db.public.coach_position_in_team (coach_id, team_id, position)
-values (3, 1, 'fitness');
+values (3, 3, 'fitness');
 insert into footballclub_db.public.coach_position_in_team (coach_id, team_id, position)
-values (4, 1, 'fitness');
+values (4, 4, 'fitness');
 insert into footballclub_db.public.coach_position_in_team (coach_id, team_id, position)
-values (5, 1, 'scout');
+values (5, 5, 'scout');
