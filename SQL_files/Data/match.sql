dd7809640f0c98e3fe5ff071074b3e317f79db9a
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-05-28 20:43:28 +0300

    match.sql updated.

--- SQL_files/Data/match.sql
+++ SQL_files/Data/match.sql
@@ -1,8 +1,8 @@
-insert into footballclub_db.public.match (match_id, home_team_id, away_team_id, match_date)
-values (1, 1, 2, '2023-05-05 14:00:00');
-insert into footballclub_db.public.match (match_id, home_team_id, away_team_id, match_date)
-values (2, 3, 4, '2023-05-06 15:00:00');
-insert into footballclub_db.public.match (match_id, home_team_id, away_team_id, match_date)
-values (3, 5, 1, '2023-05-17 17:00:00');
-insert into footballclub_db.public.match (match_id, home_team_id, away_team_id, match_date)
-values (4, 2, 3, '2023-05-20 13:00:00');
+insert into footballclub_db.public.match (match_id, home_team_id, away_team_id, match_date, match_time)
+values (1, 1, 2, '2023-05-05', '14:00:00');
+insert into footballclub_db.public.match (match_id, home_team_id, away_team_id, match_date, match_time)
+values (2, 3, 4, '2023-05-06', '15:00:00');
+insert into footballclub_db.public.match (match_id, home_team_id, away_team_id, match_date, match_time)
+values (3, 5, 1, '2023-05-17', '17:00:00');
+insert into footballclub_db.public.match (match_id, home_team_id, away_team_id, match_date, match_time)
+values (4, 2, 3, '2023-05-20', '13:00:00');
