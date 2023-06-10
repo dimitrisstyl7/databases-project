d867f357b6f9c14192733caad1b2236e3ce1832e
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-06-10 18:50:35 +0300

    footballer_statistic_in_match.sql created.

---
+++ SQL_files/Data/footballer_statistic_in_match.sql
@@ -0,0 +1,66 @@
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (1, 1, 1, 0, 1, 0, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (2, 1, 0, 1, 0, 0, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (3, 1, 0, 0, 0, 0, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (4, 1, 0, 0, 0, 0, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (5, 1, 0, 0, 0, 1, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (6, 1, 0, 0, 0, 0, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (7, 1, 0, 0, 0, 0, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (8, 1, 2, 0, 1, 1, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (9, 1, 0, 1, 0, 0, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (10, 1, 1, 0, 1, 0, 1, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (11, 1, 0, 0, 1, 0, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (12, 1, 0, 0, 0, 0, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (13, 1, 0, 1, 0, 0, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (14, 1, 1, 0, 0, 0, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (15, 1, 0, 1, 0, 0, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (16, 1, 0, 0, 0, 0, 1, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (17, 1, 0, 0, 0, 0, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (18, 1, 1, 0, 0, 0, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (19, 1, 0, 0, 0, 0, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (20, 1, 0, 0, 0, 0, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (21, 1, 0, 0, 0, 0, 0, '00:45:00');
+insert into footballclub_db.public.footballer_statistic_in_match (footballer_id, match_id, goals, corners, penalties,
+                                                                  yellow_cards, red_cards, time_played)
+values (22, 1, 0, 0, 0, 1, 0, '00:45:00');
